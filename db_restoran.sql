59573117650feea4067f8e13fba2b0ac84ec0b6d
nadiazafirazahra <114532973+nadiazafirazahra@users.noreply.github.com>
2023-02-03 12:05:41 +0700

    Add files via upload

---
+++ db_restoran.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Feb 2023 pada 05.57
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_restoran`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `masakan`
+--
+
+CREATE TABLE `masakan` (
+  `id_masakan` int(11) NOT NULL,
+  `nama_masakan` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `masakan`
+--
+
+INSERT INTO `masakan` (`id_masakan`, `nama_masakan`, `harga`, `status`) VALUES
+(1, 'rendang', 10000, 'TERSEDIA'),
+(2, 'Ayam', 12000, 'TERSEDIA'),
+(3, 'Babi guling', 100000, 'TERSEDIA'),
+(4, 'Ayam Geprek', 17000, 'TERSEDIA'),
+(5, 'Sate Kambing', 25000, 'TERSEDIA'),
+(6, 'Ayam Bakar', 15000, 'TERSEDIA');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `nama_pelanggan` varchar(30) NOT NULL,
+  `id_masakan` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama_masakan` varchar(30) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `total_bayar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `nama_user` varchar(30) NOT NULL,
+  `id_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama_user`, `id_level`) VALUES
+(1, 'admin', 'admin123', 'Fadil', 1),
+(2, 'owner', 'owner123', 'nadia', 3),
+(3, 'pelanggan', 'pelanggan123', 'juardi', 4),
+(4, 'kasir', 'kasir123', 'kanza', 2),
+(13, 'kasir2', 'kasir2', 'kasir', 1),
+(19, 'admin1', 'admin123', 'Fadil', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `masakan`
+--
+ALTER TABLE `masakan`
+  ADD PRIMARY KEY (`id_masakan`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_masakan` (`id_masakan`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `masakan`
+--
+ALTER TABLE `masakan`
+  MODIFY `id_masakan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_masakan`) REFERENCES `masakan` (`id_masakan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
